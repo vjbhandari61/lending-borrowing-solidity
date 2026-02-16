4c69fa3e7499a3e6aaba1ceac9abf5745dcc2122
vjbhandari61 <vjbhandari61@gmail.com>
2026-02-16 08:53:06 +0530

    tests: tested happy cases for deposit, borrow and repay

--- test/VaultCore.t.sol
+++ test/VaultCore.t.sol
@@ -16,20 +16,50 @@ contract VaultCoreTest is Test {
     function setUp() public {
         mock_usdc = new MockUSDC(owner, owner);
         mockUSDC = address(mock_usdc);
-        vault_core = new VaultCore("Vijay Token", "VJX", mockUSDC);
+        vault_core = new VaultCore("My Token", "MTC", mockUSDC);
     }
 
-    function test_Deposit() public {
-        // mockUSDC.mint(owner, 30 ether);
-        
-        vm.startPrank(owner);
-        IERC20(mockUSDC).approve(address(this), 30 ether);
-
+    function _deposit_helper() internal {
+        IERC20(mockUSDC).approve(address(vault_core), 30 ether);
         vault_core.deposit(30 ether);
+    }
+
+    function _borrow_helper() internal {
+        vault_core.borrow(20 ether);
+    }
+
+    function _repay_helper() internal {
+        vault_core.repay(15 ether);
+    }
+
+    function test_Deposit() public {        
+        vm.startPrank(owner);
+        _deposit_helper();
+        uint256 collateralAmt = vault_core.getUserVault(owner).collateralAmt;
+        vm.stopPrank();
+        assertEq(collateralAmt, 30 ether);
+    }
+
+    function test_Borrow() public {
+        vm.startPrank(owner);
+        _deposit_helper();
+        _borrow_helper();
+        uint256 debt = vault_core.getUserVault(owner).debt;
+        vm.stopPrank();
+        assertEq(debt, 20 ether);
+    }
+
+    function test_Repay() public {
+        vm.startPrank(owner);
+        _deposit_helper();
+        _borrow_helper();
+        _repay_helper();
+
         VaultCore.Vault memory vault = vault_core.getUserVault(owner);
         vm.stopPrank();
 
         assertEq(vault.collateralAmt, 30 ether);
+        assertEq(vault.debt, 5 ether);
     }
 
     // function testFuzz_SetNumber(uint256 x) public {
